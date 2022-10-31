6606cef0fc181d038d6ad49fec7b3152b8201d29
sirisayshello <veckovis.salongen0p@icloud.com>
2022-10-31 19:04:02 +0100

    Added index.php with styles

--- index.php
+++ index.php
@@ -1,1 +1,40 @@
-<?= 'The Elephpent in the Room'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="index.css">
+    <title>Document</title>
+</head>
+<header></header>
+
+<body>
+    <main>
+
+        <section class="startscreen">
+            <svg class="svg-stable" width="270" height="166" viewBox="0 0 270 166" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <rect x="10" y="48" width="250" height="118" rx="5" fill="#7B5F3E" />
+                <path d="M26 5C27 2.5 28.2386 0 31 0H240C242.762 0 244 2.5 245 5L270 59C270 61.7614 267.762 64 265 64H5C2.23858 64 6.19888e-06 61.7614 6.19888e-06 59L26 5Z" fill="#CE8D5E" />
+                <path d="M98 91C98 88.2386 100.239 86 103 86H163C165.761 86 168 88.2386 168 91V166H98V91Z" fill="#CE8D5E" />
+            </svg>
+            <svg class="svg-tree1" width="122" height="130" viewBox="0 0 122 130" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M51 72H66L64.2 92.88L66 130H48L51 111.15V72Z" fill="#7B5F3E" />
+                <path d="M47.8351 7.32584C25.0789 -4.2504 11.9866 8.4991 11.9866 16.712C-0.872058 24.1427 -2.56095 37.0946 3.02417 47.217C-3.21039 60.0447 11.2071 72.6378 17.8313 72.6378C31.0799 92.1922 48.3547 84.8919 60.6939 81.2417C75.6568 95.3209 102.388 80.4596 103.556 69.9001C120.39 67.0843 119.403 51.5189 116.805 44.0882C133.015 26.2546 107.064 7.32584 96.5426 12.41C78.7741 -7.30091 56.6674 0.807683 47.8351 7.32584Z" fill="#648C4C" />
+            </svg>
+
+
+            <a href="stable.php" class="big-button">GÅ IN I STALLET</a>
+
+            <svg class="svg-tree2" width="126" height="158" viewBox="0 0 126 158" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M54.8333 83H74L71.7 110L74 158H51L54.8333 133.625V83Z" fill="#7B5F3E" />
+                <path d="M73.157 6.79637C54.9813 -6.37576 29.0947 2.38603 26.685 9.73657C4.92919 9.73657 -1.19822 29.1419 4.65382 41.4908C-6.36178 50.6643 4.65384 66.4826 12.227 66.4826C8.44043 80.0075 30.5864 83.3398 41.8314 82.9477C55.0502 97.5312 73.9603 89.0242 81.763 82.9477C104.345 88.8281 109.99 71.6769 109.99 62.3663C130.094 60.7198 127.088 43.843 123.071 35.6104C129.956 25.9665 116.875 13.2648 109.99 13.2648C101.178 -3.90599 75.911 0.915926 73.157 6.79637Z" fill="#648C4C" />
+            </svg>
+
+        </section>
+
+    </main>
+</body>
+
+</html>
