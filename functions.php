8a5ad2bf88f40934a1a9ca71d2ac747af0cf207b
sirisayshello <veckovis.salongen0p@icloud.com>
2022-11-02 12:46:18 +0100

    Added files for functions and variables

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+// Fetching the chosen horse from the array horses
+function getHorseFromHorses(string $selectedHorse, array $horses): array
+{
+
+    foreach ($horses as $horse) {
+        if ($horse['name'] === $selectedHorse) {
+            return $horse;
+        }
+    }
+    // If the horse doesn't exist, default to this one.
+    return $horses[0];
+};
+
+// Fetching random results when you compete with a horse.
+function competeWithHorse(string $horseName): array
+{
+    $competitionResult =
+        [
+            ['placed' => 1, 'message' => "VINST! Vilket team! Det behöver firas. Tillbaka till stallet för morötter och tårta till dig och $horseName!"],
+            ['placed' => 2, 'message' => "En andraplats, snyggt jobbat!! Du och $horseName verkar vara ett riktigt bra team!"],
+            ['placed' => 3, 'message' => "Tjoho, 3:e placering! Du och $horseName verkar vara ett riktigt bra team!"],
+            ['placed' => 4, 'message' => "En 4:e placering löste ni galant ju! Klappa om både dig själv och $horseName!"],
+            ['placed' => 5, 'message' => "5:e placering, inte dåligt! Klappa om både dig själv och $horseName!"],
+            ['placed' => 6, 'message' => "Ja men se där, en 6:e placering! Inte din bästa prestation? Äsch, huvudsaken är att du och $horseName hade kul!"],
+            ['placed' => 'Utesluten', 'message' => "Åh nej, du blev utesluten! $horseName kanske hade en dålig dag... Men det var troligtvis ditt fel. Tillbaka till stallet och öva mer!"]
+        ];
+
+    $result = $competitionResult[array_rand($competitionResult)];
+
+    return $result;
+};
+
+
+function getHorseAge(int $yearOfBirth): int
+{
+    $currentYear = (int)date('Y');
+    $age = $currentYear - $yearOfBirth;
+
+    return $age;
+}
---
+++ variables.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+$pageTitle = "Stall Siri";
+
+$horses =
+    [
+        [
+            'name' => 'Angel',
+            'height' => '164cm',
+            'birthYear' => 2007,
+            'color' => 'Skäck',
+            'breed' => 'Irländsk Sporthäst',
+            'alt' => 'Porträtt av häst med bruna och vita fläckar med gröna träd i bakgrunden.',
+            'picture' => 'https://www5.idrottonline.se/globalassets/goteborgs-fk---ridsport/hastar/_dsc9698.jpg?w=481&h=320'
+        ],
+        [
+            'name' => 'Sunny',
+            'height' => '158cm',
+            'birthYear' => 2009,
+            'color' => 'Fux',
+            'breed' => 'Westfahler',
+            'alt' => 'Porträtt av rödbrun häst med vit bläs och skog i bakgrunden.',
+            'picture' => 'https://www5.idrottonline.se/globalassets/goteborgs-fk---ridsport/hastar/_dsc2710.jpg?w=481&h=320'
+        ],
+        [
+            'name' => 'Lincoln',
+            'height' => '175cm',
+            'birthYear' => 2009,
+            'color' => 'Brun',
+            'breed' => 'Oldenburgare',
+            'alt' => 'Porträtt av ljusbrun häst med svart man, med vitt staket och gröna träd i bakgrunden.',
+            'picture' => 'https://www5.idrottonline.se/globalassets/goteborgs-fk---ridsport/hastar/_dsc0652.jpg?w=481&h=320'
+        ],
+        [
+            'name' => 'Colleen',
+            'height' => '165cm',
+            'birthYear' => 2010,
+            'color' => 'Brun',
+            'breed' => 'Westfahler',
+            'alt' => 'Porträtt av mörkbrun häst med mörk man och gröna träd i bakgrunden.',
+            'picture' => 'https://www5.idrottonline.se/globalassets/goteborgs-fk---ridsport/hastar/_dsc9740.jpg?w=481&h=320'
+        ]
+    ];
+
+$feedMessage = "Mums!";
+
+$groomMessage = "Tack tack!";
